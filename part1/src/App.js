48fb00e8cbcac845aa9a53bcd2f48099addcbb72
Aleksi Männistö <aatumannisto@gmail.com>
2022-07-18 14:33:02 +0300

    Task 1.1.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,44 @@
-const Hello = (props) => {
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        {props.part} {props.exercise}
       </p>
+    </div>
+  );
+};
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
     </div>
   );
 };
 
 const App = () => {
-  const nimi = "Pekka";
-  const ika = 10;
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
