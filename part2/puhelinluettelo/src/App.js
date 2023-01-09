25111417f3fd260a20a4f8e1404abeed5ddeca87
Aleksi Männistö <aatumannisto@gmail.com>
2023-01-09 17:05:50 +0200

    Exercise 2.11. Phonebook step6

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newPerson, setNewPerson] = useState({ name: "", number: "" });
   const [filter, setFilter] = useState("");
-  const [visiblePersons, setVisiblePersons] = useState(persons);
+  const [visiblePersons, setVisiblePersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setVisiblePersons(response.data);
+    });
+  }, []);
 
   // Prevent form default
   const addNewPerson = (e) => {
