c0c01d179232653fdd0a6a9de81e9d199baaf518
Aleksi Männistö <aatumannisto@gmail.com>
2023-01-09 14:48:10 +0200

    Exercise 2.8. Phonebook step3

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -4,39 +4,49 @@ import Person from "./components/Person";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   // Prevent form default
-  const addNewName = (e) => {
+  const addNewPerson = (e) => {
     e.preventDefault();
 
     // Use filter to check if name is already in Phonebook
     const checkName = persons.filter((person) => person.name === newName);
 
     if (checkName.length === 0) {
-      const nameItem = {
+      const personItem = {
         name: newName,
+        number: newNumber,
       };
       // Return new array with new name(s)
-      setPersons(persons.concat(nameItem));
+      setPersons(persons.concat(personItem));
     } else {
       // Alert user that name is already in Phonebook
       alert(`${newName} is already added to phonebook`);
-      // Clear name field on submit
     }
+    // Clear name and number field on submit
     setNewName("");
+    setNewNumber("");
   };
 
-  // Update new name based on value of name field
-  const handleChange = (e) => {
+  // Update new name and number based on value of name field
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNewName}>
+      <form onSubmit={addNewPerson}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
