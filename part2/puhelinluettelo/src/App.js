9fbe751464c3176b2d61cc509638550248b94ebb
Aleksi Männistö <aatumannisto@gmail.com>
2023-01-09 14:54:30 +0200

    Exercise 2.9. Phonebook step4

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -2,9 +2,16 @@ import { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [visiblePersons, setVisiblePersons] = useState(persons);
 
   // Prevent form default
   const addNewPerson = (e) => {
@@ -20,6 +27,7 @@ const App = () => {
       };
       // Return new array with new name(s)
       setPersons(persons.concat(personItem));
+      setVisiblePersons(persons.concat(personItem));
     } else {
       // Alert user that name is already in Phonebook
       alert(`${newName} is already added to phonebook`);
@@ -38,10 +46,23 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  // Use case-insensitive filtering for names of people
+  const nameFilter = (e) => {
+    const searchName = e.target.value;
+    setFilter(searchName);
+    setVisiblePersons(
+      persons.filter((person) => person.name.toLowerCase().includes(searchName))
+    );
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with <input value={filter} onChange={nameFilter} />
+      </p>
       <form onSubmit={addNewPerson}>
+        <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
@@ -54,7 +75,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
+        {visiblePersons.map((person) => (
           <Person key={person.name} person={person} />
         ))}
       </div>
