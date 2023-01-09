595b271692b28dc0056c56515b1c01cb34cee5de
Aleksi Männistö <aatumannisto@gmail.com>
2023-01-09 14:37:38 +0200

    Exercise 2.6. Phonebook step1

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,21 +1,43 @@
 import { useState } from "react";
+import Person from "./components/Person";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  // Prevent form default
+  const addNewName = (e) => {
+    e.preventDefault();
+    const nameItem = {
+      name: newName,
+    };
+    // Return new array with new name(s), clear name field on submit
+    setPersons(persons.concat(nameItem));
+    setNewName("");
+  };
+
+  // Update new name based on value of name field
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <Person key={person.name} person={person} />
+        ))}
+      </div>
     </div>
   );
 };
